5b8398b80367a83d177ef766a9dc2f173e84fa7e
Allan Deutsch <allan@allandeutsch.com>
2025-10-07 23:25:48 -0700

    feat: export assert module and fix import extensions

--- src/assert/index.ts
+++ src/assert/index.ts
@@ -8,6 +8,6 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-export { Assert } from "./assert.ts";
-export * as Check from "./check.ts";
-export * as Is from "./check.ts";
+export { Assert } from "./assert.js";
+export * as Check from "./check.js";
+export * as Is from "./check.js";
